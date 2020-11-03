e9475d81d80941423f994ef3966b910ec7cf8dd0
jnavarrete2 <72566178+jnavarrete2@users.noreply.github.com>
2020-11-03 00:50:10 -0600

    Add files via upload
    These are the files for week 5

---
+++ Week 5 Lab Problem 1.py
@@ -0,0 +1,7 @@
+#Jesus Navarrete
+#29 October 2020
+
+#This program just prints hello world 100 times.
+
+for world in range (100):
+    print ("Hello World")
---
+++ Week 5 Lab Problem 2.py
@@ -0,0 +1,19 @@
+#Jesus Navarrete
+# 29 October 2020
+
+#This program will take a list of numbers and print them
+#on different lines
+
+
+user= [50, 60, 70]
+
+def list1 ():
+    for n in user:
+        print (n)
+list1()
+
+def list2():
+    for m in user:
+        print (m**2)
+
+list2()
