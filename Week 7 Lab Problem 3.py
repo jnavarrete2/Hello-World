211309b15f7c16773ffe7dcf1c3e404c1fe68390
jnavarrete2 <72566178+jnavarrete2@users.noreply.github.com>
2020-11-12 22:28:40 -0600

    Add files via upload
    Week 7 Lab problem about While loops and more

---
+++ Week 7 Lab Problem 3.py
@@ -0,0 +1,21 @@
+#Name of File: Week 7 Lab Problem 3
+
+#Jesus Navarrete
+#11/12/2020
+
+#This program will takes a user input, then uses a while
+#loop to check if that input is the letter A, B, or C.
+
+usr_input = input("Give either A, B, or C: ").upper()
+
+while usr_input != 'A' 'B' 'C':
+    if usr_input == 'A':
+        print("Done")
+        break
+    elif usr_input == 'B':
+        print("Done")
+        break
+    elif usr_input == 'C':
+        print("Done")
+        break
+    usr_input = input("Give either A, B, or C: ")
